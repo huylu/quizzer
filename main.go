c26a1e23dbf9261a32401a89c6cb88ced327a854
thangchung <thangchung@ymail.com>
2016-03-28 16:08:48 +0700

    init project

---
+++ main.go
@@ -0,0 +1,60 @@
+/**
+Reference at https://github.com/gin-gonic/gin/tree/master/examples/realtime-advanced
+ */
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"github.com/gin-gonic/gin"
+	"github.com/thangchung/quiz/utils"
+)
+
+const SERVER_PORT = ":6868"
+
+func main() {
+	ConfigRuntime()
+	StartWorkers()
+	StartGin()
+}
+
+func ConfigRuntime() {
+	numCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPU)
+	fmt.Printf("Running with %d CPUs\n", numCPU)
+}
+
+func StartWorkers() {
+	go utils.StatsWorker()
+}
+
+func StartGin() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.Use(utils.RateLimit, gin.Recovery())
+	router.LoadHTMLGlob("resources/*.tmpl")
+
+	router.GET("/", index)
+	router.GET("/quiz", getQuizList)
+	router.GET("/quiz/:id", getQuizById)
+
+	router.Run(SERVER_PORT)
+}
+
+func index(c *gin.Context) {
+	c.HTML(200, "index.tmpl", gin.H{
+	})
+}
+
+func getQuizList(c *gin.Context) {
+	data := gin.H {
+		"data1": "123",
+		"data2": 2,
+	}
+	c.JSON(200, data)
+}
+
+func getQuizById(c *gin.Context) {
+	c.JSON(200, c.Param("id"))
+}
---
+++ utils/network.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"fmt"
+)
+
+func RateLimit(c *gin.Context) {
+	ip := c.ClientIP()
+	value := int(ips.Add(ip, 1))
+	if value % 50 == 0 {
+		fmt.Printf("ip: %s, count %d\n", ip, value)
+	}
+
+	if value >= 200 {
+		if value % 200 == 0 {
+			fmt.Println("ip blocked")
+		}
+		c.Abort()
+		c.String(503, "you were automatically banned :0")
+	}
+}
