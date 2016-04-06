20316fc8680ca3331538093075f4ca7ead0a0c03
thangchung <thangchung@ymail.com>
2016-04-06 14:14:30 +0700

    upgrade to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,78 @@
 var path = require('path')
 var webpack = require('webpack')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const nodeEnv = process.env.NODE_ENV || 'development'
+const isProd = nodeEnv === 'production'
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    // './static/css/index.scss', // FIXME: will fix after bootstrap-material-design fixed their issue
-    './static/js/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
+  devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+  context: path.join(__dirname, './static/js'),
+  entry: {
+    js: './index.js',
+    vendor: ['react']
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new CopyWebpackPlugin([
-      {from: './node_modules/bootstrap-material-design/dist/**/*', to: './dist/libs/bootstrap-material-design'}
-    ])
-  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        loaders: ['babel'],
-        exclude: /node_modules/,
-        include: __dirname
+        test: /\.html$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]'
+        }
       },
       {
-        test: /\.scss?$/,
-        loaders: ['style', 'css', 'sass']
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          // 'react-hot',
+          'babel-loader'
+        ]
       }
     ]
   },
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, "./static/css")]
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./static/js'),
+      'node_modules'
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    })
+  ],
+  devServer: {
+    contentBase: './static/js'
+    // hot: true
   }
 }
